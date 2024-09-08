7a72d98a882393b9f62a563d9c11f3eead129ba6
melissa billard <billardmelissa1@gmail.com>
2024-09-08 15:50:15 +0200

    get customers ok

--- app/main.py
+++ app/main.py
@@ -15,7 +15,18 @@ app = FastAPI(
 # def read_root():
 #     return {"Hello": "World"}
 
+@app.get("/test_db_connection/")
+def test_db_connection(db: Session = Depends(get_db)):
+    try:
+        # Juste pour tester la connexion
+        result = db.execute("SELECT 1")
+        return {"status": "success", "result": result.fetchone()}
+    except Exception as e:
+        return {"status": "error", "details": str(e)}
+
 @app.get("/customers/", response_model=List[schemas.Customer], tags=["customers"])
 def get_customers(db: Session = Depends(get_db)):
     customers = controllers.get_customers(db)
     return customers
+
+
